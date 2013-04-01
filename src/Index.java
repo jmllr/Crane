1b97d64b50c8b0a70c8b276257ce2fb240289d42
Christian Müller <christian.s.mueller@accenture.com>
2013-04-01 16:45:20 +0200

    public writeXmlFile node test

--- src/Index.java
+++ src/Index.java
@@ -5,6 +5,19 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 // http://www.mkyong.com/tutorials/java-xml-tutorials/
 
@@ -38,12 +51,44 @@ public class Index {
              }
 
 
+
          }
 
 
          catch (Exception e) {
              e.printStackTrace();
          }
+    }
+
+
+    public void writeTestFile(){
+
+        try {
+            File xmlFile = new File("tests/index.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            Node listOfSnapshots = doc.getElementsByTagName("ListOfSnapshots").item(0);
+
+            // append a new node to ListOfSnapshots
+            Element test = doc.createElement("test");
+            test.appendChild(doc.createTextNode("test"));
+            listOfSnapshots.appendChild(test);
+
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("tests/index.xml"));
+            transformer.transform(source, result);
+
+            System.out.println("Done");
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,8 @@ public class Main {
         if (args[0].equals("test"))  {
 
             Index test = new Index();
-            test.readTestFile();
+            //test.readTestFile();
+            test.writeTestFile();
         }
     }
 }
