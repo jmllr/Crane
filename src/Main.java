7a37c72ca891006c51c7d67a017831b6900d5c1d
Christian Müller <christian.s.mueller@accenture.com>
2013-04-01 14:49:44 +0200

    first version

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        for (int i = 0; i < args.length; i++)
+        {
+
+            if (args[i] == "init")  {
+                System.out.println("Das ist init");
+            }
+
+            if (args[i] == "history")  {
+                System.out.println("Das ist history");
+            }
+
+            if (args[i] == "snapshot")  {
+                System.out.println("Das ist snapshot");
+            }
+
+            if (args[i] == "restore")  {
+                System.out.println("Das ist restore");
+            }
+
+        }
     }
 }
