ab27f41eea437535c37881fc09c6fb1402062eb4
Christian Müller <christian.s.mueller@accenture.com>
2013-04-01 15:07:04 +0200

    Array[0], String Objects

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        if (args.length == 0) {
+            System.out.println("No arguments specified!");
 
-        for (int i = 0; i < args.length; i++)
-        {
-
-            if (args[i] == "init")  {
-                System.out.println("Das ist init");
-            }
-
-            if (args[i] == "history")  {
-                System.out.println("Das ist history");
-            }
-
-            if (args[i] == "snapshot")  {
-                System.out.println("Das ist snapshot");
-            }
-
-            if (args[i] == "restore")  {
-                System.out.println("Das ist restore");
-            }
-
+            return;
         }
+
+        if (args[0].equals("init"))  {
+            System.out.println("Das ist init");
+        }
+
+        if (args[0].equals("history"))  {
+            System.out.println("Das ist history");
+        }
+
+        if (args[0].equals("snapshot"))  {
+            System.out.println("Das ist snapshot");
+        }
+
+        if (args[0].equals("restore"))  {
+            System.out.println("Das ist restore");
+        }
+
     }
 }
